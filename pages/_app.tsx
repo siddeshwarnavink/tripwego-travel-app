a6b132431936dddee71193dca218e9cf9149e0d4
Siddeshwar <siddeshwar.work@gmail.com>
2022-10-12 10:24:20 +0530

    setup mantine ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>TripWeGo</title>
+        <link rel='icon' href='/favicon.png' />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'light',
+          colors: {
+            brand: ['#d8e4b4', '#cedda1', '#c4d68f', '#bbd07c', '#b1c969', '#a7c256', '#9FBD48', '#9dbb44', '#8ea93d','#7e9636' ],
+          },
+          primaryColor: 'brand',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
-
-export default MyApp
